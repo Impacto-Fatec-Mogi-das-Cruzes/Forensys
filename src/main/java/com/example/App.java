505ba1ef41f1e37fd5e2086b320d410b1addd5c6
LucianoAk <lucianotokuno@gmail.com>
2026-04-06 20:01:52 -0300

    fix: change application icon

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -20,6 +21,8 @@ public class App extends Application {
 
         stage.setTitle("Projeto POO");
         stage.setScene(scene);
+
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
 
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setResizable(false);
