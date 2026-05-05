a027d2d3b5a7d405804b44c9b6b0ee029ff6a332
LucianoAk <lucianotokuno@gmail.com>
2026-05-04 21:03:17 -0300

    feat(ui.controller): implement ReaderController

--- src/main/java/com/forensys/ui/controller/ReaderControler.java
+++ src/main/java/com/forensys/ui/controller/ReaderControler.java
@@ -1,12 +1,18 @@
 package com.forensys.ui.controller;
 
+import com.forensys.core.context.ApplicationContext;
+import com.forensys.core.filestructure.concrete.TextFile;
 import com.forensys.ui.navigation.StageManager;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 
 public class ReaderControler {
+    @FXML
+    private TextArea content;
+
     @FXML
     private BorderPane root;
 
@@ -15,15 +21,14 @@ public class ReaderControler {
 
     @FXML
     private void initialize() {
-        root.setFocusTraversable(true);
-        root.requestFocus();
-        
-        root.setOnKeyPressed(null);
+        TextFile file = ApplicationContext.getInstance().getTextFile();
+        content.setText(file.getContent());
+        tittle.setText(file.getMetadata().name() + ".txt");
+
         root.setOnKeyReleased(event -> {
             if (event.getCode().toString().equals("Q")) {
                 StageManager.getInstance().restoreScene();
             }
         });
-        root.setOnKeyTyped(null);
     }
 }
