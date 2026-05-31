23371492e3fb6cb5b4206de424fbe52fbe4f3801
LucianoAk <lucianotokuno@gmail.com>
2026-05-30 23:28:08 -0300

    refactor: use new implementation of the CommandOutputBuilder

--- src/main/java/com/forensys/core/command/concrete/invalid/InvalidCommand.java
+++ src/main/java/com/forensys/core/command/concrete/invalid/InvalidCommand.java
@@ -3,7 +3,6 @@ package com.forensys.core.command.concrete.invalid;
 import com.forensys.core.command.CommandExitCode;
 import com.forensys.core.command.CommandMetadata;
 import com.forensys.core.command.CommandOutput;
-import com.forensys.core.command.CommandOutputBuilder;
 import com.forensys.core.command.ParsedCommandArgs;
 import com.forensys.core.command.TerminalCommand;
 
@@ -15,13 +14,10 @@ public class InvalidCommand extends TerminalCommand {
 
     @Override
     public CommandOutput execute(ParsedCommandArgs args) {
-        CommandOutputBuilder outputBuilder = new CommandOutputBuilder();
-        
-        outputBuilder
+        return CommandOutput.builder()
             .text("Invalid Command, plase input a valid command")
-            .exitCode(CommandExitCode.FAILURE);
-            
-        return outputBuilder.build();
+            .exitCode(CommandExitCode.FAILURE)
+            .build();
     }
 
 }
